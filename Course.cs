cbef4038f76334000663d5f21a233277c918b04c
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-08 17:53:10 +0000

    gg2

--- Course.cs
+++ Course.cs
@@ -1,15 +1,17 @@
-﻿namespace knightmoves;
-public class Course
-{   
+﻿namespace knightmoves
+{
+    public class Course
+    {
     // Add your code here
-    public int MaxStudents = 25;
-    public int PerfectScore = 100;
+        public int MaxStudents = 25;
+         public int PerfectScore = 100;
 
-    public int CourseLength(){
+            public int CourseLength(){
         // Add your code here
-       int courseLength = 20;
+            int courseLength = 20;
 
 
-        return courseLength;
+            return courseLength;
+        }
     }
 }
